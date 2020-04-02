4710a12a498e85d1b1d40cecea820b8e36b2588a
learnwithchris <cegan35@gmail.com>
2020-04-02 14:00:41 -0400

    Updates

---
+++ precache-manifest.dea915e25fcf7359335bbbde626651d0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be3361d6d9e24cec4dc13f19000b979",
+    "url": "/deploy-spa/index.html"
+  },
+  {
+    "revision": "65917171da3e223afb37",
+    "url": "/deploy-spa/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fe8d6cb97e6a57002bdc",
+    "url": "/deploy-spa/static/css/main.6e04f1a2.chunk.css"
+  },
+  {
+    "revision": "65917171da3e223afb37",
+    "url": "/deploy-spa/static/js/2.d11c9951.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/deploy-spa/static/js/2.d11c9951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8d6cb97e6a57002bdc",
+    "url": "/deploy-spa/static/js/main.73fa261c.chunk.js"
+  },
+  {
+    "revision": "8dd91b169709f05f332b",
+    "url": "/deploy-spa/static/js/runtime-main.768096e4.js"
+  },
+  {
+    "revision": "ecbcd91389658e7afc1f0c2620202c7e",
+    "url": "/deploy-spa/static/media/bear.ecbcd913.jpg"
+  },
+  {
+    "revision": "10468320c41207b74c3680b5e2d8beae",
+    "url": "/deploy-spa/static/media/lion.10468320.jpg"
+  },
+  {
+    "revision": "dffd85c6979ddcc6ba6cab6f6ad095e1",
+    "url": "/deploy-spa/static/media/tiger.dffd85c6.jpg"
+  }
+]);
